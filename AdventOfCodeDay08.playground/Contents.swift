e9f40fdc72cc4b2f2a7927d79c78e0dba024b3c5
Brian Prescott <bp@wave39.com>
2016-12-15 13:35:39 -0500

    Day 8 finished

--- AdventOfCodeDay08.playground/Contents.swift
+++ AdventOfCodeDay08.playground/Contents.swift
@@ -22,14 +22,18 @@ class Display {
         }
     }
     
-    func showDisplay() -> () {
+    func displayString() -> String {
+        var retVal = ""
         for r in 0..<rows {
             var rowString = ""
             for c in 0..<columns {
                 rowString += lightArray[r][c]
             }
-            print (rowString)
+            
+            retVal = retVal + rowString + "\n"
         }
+        
+        return retVal
     }
     
     func rect(w: Int, h: Int) {
@@ -39,6 +43,40 @@ class Display {
             }
         }
     }
+
+    func rotate(isRow: Bool, rcIndex: Int, rcAmount: Int) {
+        for _ in 1...rcAmount {
+            if isRow {
+                let save = lightArray[rcIndex][columns - 1]
+                for idx in (1...columns - 1).reversed() {
+                    lightArray[rcIndex][idx] = lightArray[rcIndex][idx - 1]
+                }
+                
+                lightArray[rcIndex][0] = save
+            } else {
+                let save = lightArray[rows - 1][rcIndex]
+                for idx in (1...rows - 1).reversed() {
+                    lightArray[idx][rcIndex] = lightArray[idx - 1][rcIndex]
+                }
+                
+                lightArray[0][rcIndex] = save
+            }
+        }
+    }
+    
+    func lightCounter() -> Int {
+        var ctr = 0
+        for r in 0..<rows {
+            for c in 0..<columns {
+                if lightArray[r][c] == "#" {
+                    ctr += 1
+                }
+            }
+        }
+        
+        return ctr
+    }
+
 }
 
 let puzzleInputPath = Bundle.main.path(forResource: "puzzle_input", ofType: "txt")
@@ -46,7 +84,7 @@ let puzzleInputData = FileManager.default.contents(atPath: puzzleInputPath!)
 let puzzleInputString = String(data: puzzleInputData!, encoding: .utf8)
 let puzzleInputLineArray = puzzleInputString?.components(separatedBy: "\n").filter { $0.characters.count > 0 }
 
-var part1Display = Display(r: 3, c: 7)
+var part1Display = Display(r: 6, c: 50)
 for line in puzzleInputLineArray! {
     let arr = line.components(separatedBy: " ")
     if arr.count > 0 {
@@ -55,10 +93,15 @@ for line in puzzleInputLineArray! {
             let rectWidth = Int(rectArr[0])
             let rectHeight = Int(rectArr[1])
             part1Display.rect(w: rectWidth!, h: rectHeight!)
-            
         } else if arr[0] == "rotate" {
+            let rotateRow = (arr[1] == "row")
+            let index = Int(arr[2].replacingOccurrences(of: "x=", with: "").replacingOccurrences(of: "y=", with: ""))
+            let amount = Int(arr[4])
+            part1Display.rotate(isRow: rotateRow, rcIndex: index!, rcAmount: amount!)
         }
     }
 }
-part1Display.showDisplay()
 
+// press Command+Shift+Y to display the console output, where the displayString method will be readable
+print (part1Display.displayString())
+print (part1Display.lightCounter(), terminator: "")
