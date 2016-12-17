da7e51396df27db1fc1200f33d07a4306e405e27
Brian Prescott <bp@wave39.com>
2016-12-16 20:12:00 -0500

    Day 10 WIP

--- AdventOfCodeDay10.playground/Contents.swift
+++ AdventOfCodeDay10.playground/Contents.swift
@@ -4,7 +4,8 @@
 
 import Cocoa
 
-let puzzleInputPath = Bundle.main.path(forResource: "puzzle_input", ofType: "txt")
+let puzzleInputPath = Bundle.main.path(forResource: "puzzle_input_test", ofType: "txt")
+//let puzzleInputPath = Bundle.main.path(forResource: "puzzle_input", ofType: "txt")
 let puzzleInputData = FileManager.default.contents(atPath: puzzleInputPath!)
 let puzzleInputString = String(data: puzzleInputData!, encoding: .utf8)
 let puzzleInputLineArray = puzzleInputString?.components(separatedBy: "\n").filter { $0.characters.count > 0 }
@@ -16,6 +17,34 @@ struct BotInfo {
         chips = []
         lastComparison = []
     }
+}
+
+func findBotReadyToProceed(botArray: Dictionary<Int, BotInfo>) -> Int {
+    for k in botArray.keys {
+        if botArray[k]?.chips.count == 2 {
+            return k
+        }
+    }
+
+    return -1
+}
+
+func findBotWithLastComparison(botArray: Dictionary<Int, BotInfo>, i1: Int, i2: Int) -> Int {
+    return -1
+}
+
+func findBotInstructions(botNumber: Int) -> [String] {
+    for line in puzzleInputLineArray! {
+        let arr = line.components(separatedBy: " ")
+        if arr[0] == "bot" {
+            let b = Int(arr[1])!
+            if b == botNumber {
+                return arr
+            }
+        }
+    }
+
+    return []
 }
 
 print (puzzleInputLineArray!)
@@ -31,8 +60,27 @@ for line in puzzleInputLineArray! {
         if part1Bots[botNumber] == nil {
             part1Bots[botNumber] = BotInfo()
         }
+        
         part1Bots[botNumber]?.chips.append(chipNumber)
     }
 }
 
 print (part1Bots)
+
+var part1 = findBotWithLastComparison(botArray: part1Bots, i1: 2, i2: 5)
+var runaway = 0
+while part1 == -1 {
+    let foundBot = findBotReadyToProceed(botArray: part1Bots)
+    if foundBot == -1 {
+        print ("No bot was found ready to proceed!")
+    }
+    
+    let arr = findBotInstructions(botNumber: foundBot)
+    
+    runaway += 1
+    if runaway > 100 {
+        part1 = 88888
+    }
+}
+
+print (part1)
