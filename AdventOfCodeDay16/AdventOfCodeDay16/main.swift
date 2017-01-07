20ea6405f04b15470583cbdb618746f4297f52e4
Brian Prescott <bp@wave39.com>
2017-01-06 21:53:25 -0500

    Day 16 finished

--- AdventOfCodeDay16/AdventOfCodeDay16/main.swift
+++ AdventOfCodeDay16/AdventOfCodeDay16/main.swift
@@ -2,56 +2,79 @@
 // Day 16
 // http://adventofcode.com/2016/day/16
 
+// BTW: Swift string operations are SLOOOOOOOOOOOOOOOOOOOOOW !!!!!!!!!!!!!!
+
 import Cocoa
 
-func createFillString(str: String) -> String {
-    let a = str
-    let b = String(str.characters.reversed())
-    let b1 = b.replacingOccurrences(of: "1", with: "2")
-    let b2 = b1.replacingOccurrences(of: "0", with: "1")
-    let b3 = b2.replacingOccurrences(of: "2", with: "0")
-    let c = a + "0" + b3
-    print (c.characters.count)
-    return c
-}
-
-func createDiscState(initialString: String, size: Int) -> String {
-    var s = initialString
-    while s.characters.count < size {
-        s = createFillString(str: s)
-    }
-    
-    return s.substring(to: size)
-}
-
-func generateChecksum(str: String) -> String {
-    var s = str
-    var sLength = s.length
-    while sLength % 2 == 0 {
-        var s2 = ""
-        for idx in stride(from: 0, to: sLength - 1, by: 2) {
-            if s[idx] == s[idx + 1] {
-                s2 += "1"
-            } else {
-                s2 += "0"
-            }
-        }
-        
-        s = s2
-        sLength = s.length
+func arrayDescription(arr: [Int]) -> String {
+    var s = ""
+    for i in arr {
+        s += "\(i)"
     }
     
     return s
 }
 
-func processInput(input: String, length: Int) -> String {
-    let discData = createDiscState(initialString: input, size: length)
-    return generateChecksum(str: discData)
+func createFillData(data: [Int]) -> [Int] {
+    let a = data
+    let b = data.reversed()
+    let b1 = b.map { ($0 == 1 ? 2 : $0) }
+    let b2 = b1.map { ($0 == 0 ? 1 : $0) }
+    let b3 = b2.map { ($0 == 2 ? 0 : $0) }
+    let c = a + [ 0 ] + b3
+    return c
 }
 
-//let part1Input = "10000"
-//let part1Length = 20
-let part1Input = "10001110011110000"
+func createDiscState(initialData: [Int], size: Int) -> [Int] {
+    var d = initialData
+    while d.count < size {
+        d = createFillData(data: d)
+    }
+    
+    if d.count > size {
+        var d2: [Int] = []
+        for idx in 0..<size {
+            d2.append(d[idx])
+        }
+        
+        return d2
+    }
+    
+    return d
+}
+
+func generateChecksum(data: [Int]) -> [Int] {
+    var d = data
+    var dCount = d.count
+    while dCount % 2 == 0 {
+        var s2: [Int]  = []
+        for idx in stride(from: 0, to: dCount - 1, by: 2) {
+            if d[idx] == d[idx + 1] {
+                s2.append(1)
+            } else {
+                s2.append(0)
+            }
+        }
+        
+        d = s2
+        dCount = d.count
+    }
+    
+    return d
+}
+
+func processInput(input: [Int], length: Int) -> String {
+    let discData = createDiscState(initialData: input, size: length)
+    let checksum = generateChecksum(data: discData)
+    return arrayDescription(arr: checksum)
+}
+
+let testInput = [ 1, 0, 0, 0, 0 ]
+let testLength = 20
+let testSolution = processInput(input: testInput, length: testLength)
+print ("Test solution: \(testSolution)")
+
+let part1Input = [1, 0, 0, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0 ]
 let part1Length = 272
 let part1Solution = processInput(input: part1Input, length: part1Length)
 print ("Part 1 solution: \(part1Solution)")
